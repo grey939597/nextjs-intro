ccec460bf2f15bbd7a335d5ad10f15773c3987c9
grey939597 <jongyunj@icloud.com>
2022-01-16 20:47:09 +0900

    #2.0 Patterns

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Seo from "../components/seo";
+
 export default function About() {
   return (
     <>
+      <Seo title="About" />
       <h1>About</h1>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Seo from "../components/seo";
+
 export default function Home() {
   return (
     <>
+      <Seo title="Home" />
       <h1>Hello</h1>
     </>
   );
