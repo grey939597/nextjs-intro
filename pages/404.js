e06261033d547d1bdab0e6e449cb1ae350538a53
grey939597 <jongyunj@icloud.com>
2022-01-16 23:09:56 +0900

    #2.8 404 Pages

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <h3>what are u doing here?</h3>
+      <style jsx>{`
+        h3 {
+          margin: 20px auto;
+          text-align: center;
+        }
+      `}</style>
+    </>
+  );
+}
